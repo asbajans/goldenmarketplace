4c192810d0f05bf332613bda5232adf27ea4dc66
Golden Crafter <you@example.com>
2026-04-13 20:19:22 +0300

    sss

--- backend/testdb.ts
+++
@@ -1,5 +0,0 @@
-const sequelize = require('./src/config/database').default;
-sequelize.query('SELECT id, title, marketplaces FROM products LIMIT 5').then((res) => {
-  console.log(res[0]);
-  process.exit(0);
-});
--- backend/update.ts
+++
@@ -1,5 +0,0 @@
-const sequelize = require('./src/config/database').default;
-sequelize.query(`UPDATE products SET marketplaces = '["Golden Marketplace"]' WHERE id = '454ec437-8f1d-45e8-b654-132a1a179d2f'`).then(() => {
-  console.log("Updated DB");
-  process.exit(0);
-});
